f9346997ba6f6e4ddb90e1303a7bae8a51611ce3
alswn8972 <alswn8972@daum.net>
2021-11-30 09:22:39 +0900

    Feat : Frontend Init Setting, MainPage

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="Nav">
+    <v-card>
+    <v-toolbar
+      color="cyan"
+      flat
+    >
+
+      <v-toolbar-title>SITE Check</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          align-with-title
+        >
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab
+            v-for="item in items"
+            :key="item"
+          >
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+        tab: null,
+        items: [
+          'web', 'shopping', 'videos', 'images', 'news',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+  }
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vuetify from 'vuetify'		// <-- 추가
+import 'vuetify/dist/vuetify.min.css'
+import router from './router/router.js';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+//ßimport store from './store'
 Vue.config.productionTip = false
+Vue.use(Vuetify);	
 
 new Vue({
+  router,
+  //store,
+  vuetify : new Vuetify(),
   render: h => h(App),
+  icons: {
+    iconfont: 'md',
+  },
 }).$mount('#app')
