e7df93fdcef99eddb0394b6911366b5857f1fff1
alswn8972 <alswn8972@daum.net>
2021-12-01 17:52:11 +0900

    Feat : Add Login,SignUp Button
    페이지 네비 바에 로그인, 회원가입 버튼을 추가함

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,69 +1,54 @@
 <template>
-<div class="Nav">
-    <v-card>
-    <v-toolbar
-      color="cyan"
-      flat
-    >
+    <div class="Nav">
+        <v-card>
+            <v-toolbar color="cyan" flat="flat">
 
-      <v-toolbar-title>SITE Check</v-toolbar-title>
+                <v-toolbar-title>SITE Monitor</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-
-      <template v-slot:extension>
-        <v-tabs
-          v-model="tab"
-          align-with-title
-        >
-          <v-tabs-slider color="yellow"></v-tabs-slider>
-
-          <v-tab
-            v-for="item in items"
-            :key="item"
-          >
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-      </template>
-    </v-toolbar>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-        v-for="item in items"
-        :key="item"
-      >
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-btn
+                    class="login_btn"
+                    @click="openDialog()"
+                    outlined="outlined"
+                    color="indigo">
+                    Login
+                </v-btn>
+                <v-btn class="sign_btn" outlined="outlined" color="indigo">
+                    SignUp
+                </v-btn>
+            </v-toolbar>
         </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
+        <Login v-if="this.dialog"/>
 
-</div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "Nav",
-  props: {
-    msg: String,
-  },
-  data(){
-    return {
-        tab: null,
-        items: [
-          'web', 'shopping', 'videos', 'images', 'news',
-        ],
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      }
-  }
-};
+    import Login from "./Login.vue"
+    export default {
+        name: "Nav",
+        components: {
+            Login
+        },
+        props: {},
+        data() {
+            return {
+                dialog:false,
+            };
+        },
+        methods: {
+            openDialog() {
+                console.log("click");
+                this.dialog = true;
+            }
+        }
+    };
 </script>
+
+<style>
+    .login_btn {
+        margin-right: 20px;
+    }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,17 +4,14 @@ import App from './App.vue'
 import Vuetify from 'vuetify'		// <-- 추가
 import 'vuetify/dist/vuetify.min.css'
 import router from './router/router.js';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
 //ßimport store from './store'
 Vue.config.productionTip = false
 Vue.use(Vuetify);	
-
+Vue.component('menu-icon', MenuIcon);
 new Vue({
   router,
   //store,
   vuetify : new Vuetify(),
   render: h => h(App),
-  icons: {
-    iconfont: 'md',
-  },
 }).$mount('#app')
