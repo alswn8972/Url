44e220c9317ac87ccf3b2de0c1fb62254979575f
alswn8972 <alswn8972@daum.net>
2021-12-02 17:34:36 +0900

    Feat : Add MainPage, JoinPage, NavBar Menu
    메인화면(네비바 메뉴 추가), 회원가입 페이지 추가

---
+++ frontend/src/components/common/menu.vue
@@ -0,0 +1,23 @@
+<template v-slot:extension>
+    <v-tabs align-with-title="align-with-title">
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab v-for="item in items" :key="item">
+            {{ item }}
+        </v-tab>
+    </v-tabs>
+</template>
+
+<script>
+    export default {
+    data () {
+      return {
+        tab: null,
+        
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
+  }
+</script>
+
+<style></style>
